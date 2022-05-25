292a367c7da1bb3d3b5f9f5b9c1b0cc811b555e1
nanaki610 <78395120+nanaki610@users.noreply.github.com>
2022-05-25 14:02:44 +0900

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// import "../css/styles.css";
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "src/js/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
